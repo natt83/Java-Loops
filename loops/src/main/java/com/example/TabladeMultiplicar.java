84d5b7cd853b9eeaa40592d80de8572637b80a7f
Natt83~ <natimetal83@gmail.com>
2024-01-18 11:27:16 +0100

    fixed committed

--- loops/src/main/java/com/example/TabladeMultiplicar.java
+++ loops/src/main/java/com/example/TabladeMultiplicar.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class TabladeMultiplicar {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in)
-        System.out.print("1")
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("1");
         int numero = scanner .nextInt();
 
-        for ( int i = 1; i = <=10; i ++){
+        for ( int i = 1; i = <= 10; i ++){
             int resultado = numero * i;
             System.out.println(numero + " x " + i + " = " + resultado);
 
