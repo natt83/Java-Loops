35777cda3e0d86d1040068ac17e176860c792dd7
Natalia GF <natimetal83@gmail.com>
2024-01-19 09:08:28 +0100

    tabla-de-multiplicar

---
+++ loops/src/main/java/com/example/TablaMultiplicar.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class TablaMultiplicar {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= 10; i++) {
+            int resultado = n * i;
+            System.out.println(n + " x " + i + " = " + resultado);
+        }
+    }
+}
